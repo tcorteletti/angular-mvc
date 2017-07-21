8bd88ff2e1b81626c99977098c453c87969a7f47
Thiago Corteletti <tcorteletti@gmail.com>
2017-07-21 15:55:50 -0300

    alteracao no servidor

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http')
     ,app = require('./config/express')
 
-http.createServer(app).listen(3000, function() {
-    console.log('Servidor estutando na porta: ' + this.address().port);
-});
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
+http.createServer(app).listen(server_port, server_ip_address, function() {
+    console.log('Servidor ' + this.address().address + ' estutando na porta: ' + this.address().port);
+});
